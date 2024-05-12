be2f4d1e41dee1477173e7939cdfea09e2957e99
Ahosan-1995 <ahsoanhabibshakil2@gmail.com>
2024-05-12 10:11:54 +0600

    Projects initial setup and footer and nav complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+import Root from "../Layout/Root";
+import ErrorPage from "../Shared/ErrorPage";
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>,
+            },
+        ]
+    }
+]);
+
+export default router;
