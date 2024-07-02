6dfa4d9e48bef2aefc7ba6df6e0e56ca99c396c9
Ahosan-1995 <ahsoanhabibshakil2@gmail.com>
2024-07-02 16:42:15 +0600

    Change Redme.md file

--- README.md
+++ README.md
@@ -6,13 +6,19 @@ Live site:
 
 - [GitHubLink](https://github.com/Porgramming-Hero-web-course/b9a11-client-side-Ahosan-1995)
 
+- Project Overview:
+This website is base on beauty care services where user are able to login and booking their appointment as well as can see the services that provide by service provider
+
 
 Features of this site:
 - Selected category-assignment_category_0002 and Sub category Beauty and Grooming Services
 - Single page application loaded data efficiently
 - Full navigation control on the home page.
-- Data will be loaded very first and accurately.
-- All the content placement is well structured.
 - You can login and access our full site
 - For better user experience all different elements are seperated in different route.
 - Responsive design.
+
+
+
+Dependencies To run this project locally:
+- React, React router, Tailwind CSS, Daisy UI, Firebase and have to change firebase key
--- src/Shared/SearchBar.jsx
+++ src/Shared/SearchBar.jsx
@@ -1,39 +1,45 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar = ({setResults}) => {
-
+const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
-
-
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
+  const fatchData = (value) => {
+    fetch("https://assignment-11-serversite.vercel.app/assignment")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        const result = data.filter((user) => {
+          return (
+            user &&
+            user.service_name &&
+            user.service_name.toLowerCase().includes(value)
+          );
+        });
 
-  const fatchData=(value)=>{
-        fetch('https://assignment-11-serversite.vercel.app/assignment')
-        .then(res=>res.json())
-        .then(data=>{
-            // console.log(data);
-            const result = data.filter((user)=>{
-                return user && user.service_name && user.service_name.toLowerCase().includes(value);
-            });
-            // setInput(data);
-            // console.log(result);
-            setResults(result);
+        const result2 = data.filter((user) => {
+          return user?.price && String(user.price).includes(value);
+        });
+
+        // setInput(data);
+        // console.log(result);
+        const finalResults= ()=>{
+          return result || result2
+        }
 
 
+        setResults(finalResults);
 
-            setDropdownOpen(true);
-        })
+        setDropdownOpen(true);
+      });
+  };
 
-  }
-
-  const handleChange =(value)=>{
+  const handleChange = (value) => {
     setInput(value);
     fatchData(value);
-    
-  }
+  };
 
   return (
     <div className="relative">
@@ -42,10 +48,7 @@ const SearchBar = ({setResults}) => {
           className="w-50 bg-white border rounded-lg text-black"
           type="text"
           value={input}
-          onChange={(e)=>handleChange(e.target.value)}
-
-
-
+          onChange={(e) => handleChange(e.target.value)}
           onFocus={() => setDropdownOpen(true)}
           onBlur={() => setTimeout(() => setDropdownOpen(false), 100)}
         />
